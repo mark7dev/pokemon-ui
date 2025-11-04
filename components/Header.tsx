3a5ad4000df53da74eeff7452abed3f700c0dc56
mark7dev <mark7dev@outlook.com>
2025-11-04 05:06:29 -0600

    add logo to Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,25 @@
 "use client";
 
+import Image from "next/image";
 import { ThemeToggle } from "./ThemeToggle";
 
 export const Header = () => {
     return (
-        <header className="w-full p-4 flex justify-end">
-            <ThemeToggle />
+        <header className="w-full p-4 flex justify-between items-center pt-6 pb-6">
+            <div className="flex-1"></div>
+            <div className="flex-1 flex justify-center">
+                <Image
+                    src="/logo.png"
+                    alt="Pokemon Logo"
+                    width={320}
+                    height={240}
+                    priority
+                    className="h-auto"
+                />
+            </div>
+            <div className="flex-1 flex justify-end">
+                <ThemeToggle />
+            </div>
         </header>
     )
 }
